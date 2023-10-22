579a51d55841b1ec50e619ccd94e16063ddf8957
ShirleyL3 <144644338+ShirleyL3@users.noreply.github.com>
2023-10-21 23:35:54 -0400

    Update Midtermsketch.js

--- Midtermsketch.js
+++ Midtermsketch.js
@@ -37,3 +37,27 @@ function strawberry(){
   fill()
 
 }
+let startColor = 255;
+let speedAnimate = 1.5;
+function setup() {
+  noStroke();
+}
+
+function draw() {
+  background(255);
+  
+  if(startColor > width + 255 || startColor < 255) {
+    speedAnimate *= -1;
+  }
+  startColor += speedAnimate;
+  
+  let gap = -12;
+  let numRectangles = 30;
+  let rectWidth = width / numRectangles;
+  for (let x = 0; x < width; x += gap + rectWidth) {
+    let green = startColor - x;
+    let red = 255;
+    fill(red, green, 0);
+    rect(x, 0, rectWidth, height)
+  }
+}
