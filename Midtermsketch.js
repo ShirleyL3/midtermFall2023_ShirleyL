4ecfa98118eefa503d494532b3df862f6c54f8a1
ShirleyL3 <144644338+ShirleyL3@users.noreply.github.com>
2023-10-21 23:34:29 -0400

    Background Changes
    I want a gradient background so I am experimenting with this code to see which would create the effects I am looking for.

--- Midtermsketch.js
+++ Midtermsketch.js
@@ -1,13 +1,39 @@
-function setup() {
- //
- createCanvas(800,800);
- background(50);
-}
-
 // chaning gradient in the back 
 // food shapes: strawberry, grapes, cheese 
 //bite effect on fruit 
+// yummy face? smiley with tounge?
+
+
+let dim;
+
+function setup() {
+  createCanvas(800,800);
+  background(255);
+  colorMode(HSB, 360, 100, 100);
+  dim = width /2
+  noStroke();
+  ellipseMode(RADIUS);
+  frameRate(1);
+}
 
 function draw() {
-  // put drawing code here
+  background(0);
+  for (let x = 0; x <= width; x += dim) {
+    drawGradient(x, height / 2);
+  }
+}
+
+function drawGradient(x, y) {
+  let radius = dim / 2;
+  let h = random(0, 360);
+  for (let r = radius; r > 0; --r) {
+    fill(h, 90, 90);
+    ellipse(x, y, r, r);
+    h = (h + 1) % 360;
+  }
+}
+
+function strawberry(){
+  fill()
+
 }
