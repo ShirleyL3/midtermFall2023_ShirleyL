010afbbeac26b6f05e2fd27644a2e94abb9dabc5
ShirleyL3 <144644338+ShirleyL3@users.noreply.github.com>
2023-11-07 00:18:29 -0500

    Final Forked Midterm

--- Midtermsketch.js
+++ Midtermsketch.js
@@ -10,13 +10,18 @@ var col = {
 let x;
 let y;
 let berry;
+let cheese1;
+let strawberry1; 
 let scene = 1;
 let cheeses = [];
 let strawberrys = [];
 
+
 function setup() {
   createCanvas(800,800);
   berry = new Strawberry(30,40);
+  strawberry1 = new Strawberry(150,600);
+  cheese1 = new Cheese(600,630 );
 
   for (let i =0; i<10; i++){ // adds new objects to strawberrys array at different positions
     x = random(width);
@@ -41,28 +46,26 @@ function draw() {
       strawberrys[i].move();
       strawberrys[i].display();
     
-    var d = dist(cheeses[i].x, cheeses[i].y, strawberrys[i].x, strawberrys[i].y);//calculates distance
+      var d = dist(cheeses[i].x, cheeses[i].y, strawberrys[i].x, strawberrys[i].y);//calculates distance
       
-    if ( d < cheeses[i].r + strawberrys[i].r){ // if a cheese and strawberry are touching then change coloruses radius distance to check
-        cheeses[i].changecol();
-        strawberrys[i].changecol();
-    }
+      if ( d < cheeses[i].r + strawberrys[i].r){ // if a cheese and strawberry are touching then change color uses radius distance to check
+          cheeses[i].changecol();
+          strawberrys[i].changecol();
+      }
+      cheeses[i].display();
+      strawberrys[i].display();
     }
   }
+  
+  if (scene == 2){ //Huge strawberry getting bit
+    mousePressed();
+    }
 
-  if (scene == 2){// calls flavor_dots function with chef_ratatouille
+  if (scene == 3){// calls flavor_dots function with chef_ratatouille
     flavor_dots();
     chef_ratatouille();
-  }
-
-  if (scene == 3){ //Huge strawberry getting bit
-    mousePressed();
-   }
-}
-
-function mousePressed(){
-  if (scene ==3){
-    eat();
+    cheese1.display();
+    strawberry1.display();
   }
 }
 
@@ -71,24 +74,25 @@ function eat(){ // variety of bite marks based on position
   
   translate(mouseX, mouseY);
   if ((mouseX< 400) && (mouseY<=400)){ //Top Left
-    print("1");
+    // print("1");
     scale(1.2 );
     bite(0,0);
   } 
   if ((mouseX > 400 ) && (mouseY<=400)){ //Top Right
-    print("2");
-    scale(0.5);
-    bite(0,0);  } 
+    // print("2");
+    scale(1);
+    bite2(0,0);
+  } 
   if ((mouseX< 400) && (mouseY>=400)){ //Bottom Left
-    print("3");
+    // print("3");
     scale(0.7);
-    bite(0,0);
+    bite2(0,0);
   }
   if((mouseX> 400) && (mouseY>=400)){ //Bottom Right
-    print("4");
-    bite(0,0);
+    // print("4");
+    bite (0,0);
   }
-  print("mouseX", mouseX, "mouseY", mouseY);
+  // print("mouseX", mouseX, "mouseY", mouseY);
 pop();
 }
 
@@ -116,15 +120,23 @@ function bite(x,y){ // Bite Mark
   circle(x+30, y +50, 50);
   circle(x,y +30,50);
 }
-
-function ratatouille(x, y){ // Ratatouille Mouse Shape
-  fill("#A8A8A8"); //Grey
-  ellipse(x, y, 50, 70);
-  fill("#EEA8A8") //pink
-  circle(x, y-40, 15);
-  stroke("#EEA8A8"); //pink
-  bezier(x, y+34, x, y+80, x-10, y+82, x-12, y+85);
+function bite2(x,y){ //Bite Mark 2
+  noStroke();
+  fill(0);
+  triangle(x, y, x+80, y, x+40, y+70);
+  triangle(x, y+50, x+80, y+50, x+40, y-30);
 }
+
+//Wanted to use this to translate under the mouse but it kept printing along with the bite marks so I was
+//not sure how to implement this
+// function ratatouille(x, y){ // Ratatouille Mouse Shape
+//   fill("#A8A8A8"); //Grey
+//   ellipse(x, y, 50, 70);
+//   fill("#EEA8A8") //pink
+//   circle(x, y-40, 15);
+//   stroke("#EEA8A8"); //pink
+//   bezier(x, y+34, x, y+80, x-10, y+82, x-12, y+85);
+// }
 
 function chef_ratatouille(){
   fill("#EEA8A8"); //pink ears
@@ -183,6 +195,7 @@ class Strawberry{ // Strawberry Class
     this.r = 55;
     this.col = color(149,0,0);//Red
     this.col2 = color(41,183, 84); // Green
+    this.col3 = color(0); // black
   }
   display(){ //Displays strawberry Shape
     noStroke();
@@ -194,6 +207,14 @@ class Strawberry{ // Strawberry Class
     triangle(this.x +50, this.y, this.x +70, this.y -25, this.x +70, this.y);
     fill(0,0,0,0);// Transparent circle mianly using it to calculate the distance with this.r 
     ellipse(this.x +50, this.y +35, this.r*2);
+    // seeds
+    fill(this.col3);
+    ellipse(this.x +25 , this.y+20, 5, 10);
+    ellipse(this.x + 50, this.y +20, 5,10);
+    ellipse(this.x + 75, this.y +20, 5,10);
+    ellipse(this.x + 37, this.y +40, 5,10);
+    ellipse(this.x + 60, this.y +40, 5,10);
+    ellipse(this.x + 50, this.y +60, 5,10);
   }
   move(){ // moves along the x-axis
     this.x = this.x + random(-5,5);
@@ -202,10 +223,17 @@ class Strawberry{ // Strawberry Class
   changecol() { //random color
     this.col = color(random(255), random(255), random(255));
     this.col2 = color(random(255), random(255), random(255));
+    this.col3 = color(random(255), random(255), random(255));
   }
   huge(){// makes strawberry bigger calling the display method
     scale(5.0);
     this.display();
+  }
+}
+
+function mousePressed(){
+  if (scene ==2){
+    eat();
   }
 }
 
@@ -214,10 +242,10 @@ function keyPressed(){ //Changes Scene
 
   if (scene ==2){ //resets background before scene 2
     background(0);
+    berry.huge(); // makes the berry part of the background so bites can overlap it
   }
   if (scene ==3){ //resets background before scene 2
     background(0);
-    berry.huge(); // makes the berry part of the background so bites can overlap it
   }
   if (scene > 3 ){
     scene = 1;// resets to first scene
